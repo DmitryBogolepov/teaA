6108d2e712d89b73e88e155c41945d0bb20f1d2e
Дмитрий <dimabogolepov2002@mail.ru>
2025-05-28 10:44:23 +0300

    feat:created all components needed for project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainComponent} from "./pages/main/main.component";
+import {OrderComponent} from "./pages/order/order.component";
+import {CatalogComponent} from "./pages/catalog/catalog.component";
+import {ProductPageComponent} from "./pages/product-page/product-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:MainComponent},
+  {path:'order', component:OrderComponent},
+  {path:'catalog', component:CatalogComponent},
+  {path:'product', component:ProductPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import { MainComponent } from './pages/main/main.component';
+import { OrderComponent } from './pages/order/order.component';
+import { CatalogComponent } from './pages/catalog/catalog.component';
+import { ProductPageComponent } from './pages/product-page/product-page.component';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    OrderComponent,
+    CatalogComponent,
+    ProductPageComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
